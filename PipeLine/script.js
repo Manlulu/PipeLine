afb13e66e645d3ceaa709d51295b5e3e81e719b8
Kim <fletn3000@hotmail.com>
2016-12-14 20:25:50 +0100

    Movement to player added

---
+++ PipeLine/script.js
@@ -0,0 +1,90 @@
+var canvas = document.getElementById("game_board");
+var context = canvas.getContext("2d");
+
+var gridSize = {cols: 15, rows: 15};
+var playerSize = {width: Math.ceil(canvas.width / gridSize.cols), height: Math.ceil(canvas.height / gridSize.rows)};
+var player = {};    // Player skal inneholde x og y coordinater
+var playerDirection;
+
+var MovingDirection = {
+    LEFT: 0,
+    UP: 1,
+    RIGHT: 2,
+    DOWN: 3,
+    STAND_STILL: 4
+};
+
+init();
+
+function init() {
+    startGame();
+}
+
+function startGame() {
+    player.x = 2;
+    player.y = 4;
+
+    playerDirection = MovingDirection.STAND_STILL;
+    gameLoop();
+}
+
+function gameLoop() {
+    update();
+    draw();
+
+    setTimeout(gameLoop, 1000 / 5);
+}
+
+function update() {
+    movePlayer();
+}
+
+function draw() {
+    // Reset canvas
+    context.fillStyle = "#2782D2";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    // Draw player
+    context.fillStyle = "#C3E172";
+    context.fillRect(player.x * playerSize.width, player.y * playerSize.height,
+        canvas.width / gridSize.cols, canvas.height / gridSize.rows);
+}
+
+function movePlayer() { // Want movement and stopping in the same function
+    var checkDirection = playerDirection;
+    playerDirection = MovingDirection.STAND_STILL;
+    switch (checkDirection){
+        case MovingDirection.LEFT:
+            player.x--;
+            break;
+        case MovingDirection.UP:
+            player.y--;
+            break;
+        case MovingDirection.RIGHT:
+            player.x++;
+            break;
+        case MovingDirection.DOWN:
+            player.y++;
+            break;
+    }
+}
+
+document.addEventListener('keydown', function(event){
+    switch (event.keyCode){
+        case 37:
+            playerDirection = MovingDirection.LEFT;
+            break;
+        case 38:
+            playerDirection = MovingDirection.UP;
+            break;
+        case 39:
+            playerDirection = MovingDirection.RIGHT;
+            break;
+        case 40:
+            playerDirection = MovingDirection.DOWN;
+            break;
+        default:
+            playerDirection = MovingDirection.STAND_STILL;
+    }
+});
+
